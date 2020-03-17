7900d1c8fde5d6b26b4f26673ecfea10d5d20d3c
Gazder Bence <bencegazder@gmail.com>
2020-03-17 13:48:10 +0100

    Add login method

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
 import router from './router'
+ 
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
+
+Vue.axios.defaults.baseURL = process.env.VUE_APP_API_URL
+Vue.axios.defaults.withCredentials = true;
 
 new Vue({
   router,
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <input type="text" v-model="credentials.email" placeholder="email"/>
+        <input type="password" v-model="credentials.password" placeholder="password"/>
+        <button @click="sendLogin">LOGIN</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data () {
+        return {
+            credentials: {
+              email: '',
+              password: '',
+            }
+        }
+    },
+    methods: {
+        sendLogin() {
+          this.axios.get('airlock/csrf-cookie').then(response => {
+            console.log(response);
+
+            this.axios.post('auth/login', this.credentials).then((data) => {
+              console.log('LOGIN SUCCESFUL')
+              console.log(data);
+            }).catch((exception) => {
+                console.log('Login unsuccesful');
+                console.error(exception)
+            });
+          });
+        }
+    }
+}
+</script>
